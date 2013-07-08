cd951f924f9c36a997ffff6bffe481d8fa88bd05
Robson Knabben <robson.knabben@gmail.com>
2013-07-08 03:32:56 -0300

    Add projeto

---
+++ TCCweb/core/admin.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+from django.utils.datetime_safe import datetime
+from django.utils.translation import ungettext, ugettext as _
+from django.contrib import admin
+from TCCweb.core.models import Estabelecimento, Produto, Linha, Telefone, Ramo, UserProfile
+from django.contrib.contenttypes import generic
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class TelefoneAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'estabelecimento')
+    search_fields = ('numero',)
+
+    def queryset(self, request):
+        return super(TelefoneAdmin, self).queryset(request).filter(estabelecimento=request.user.get_profile().estabelecimento)
+
+    def save_model(self, request, obj, form, change):
+        obj.estabelecimento=request.user.get_profile().estabelecimento
+        obj.save()
+
+
+class TelefoneInline(admin.TabularInline):
+    model = Telefone
+    extra = 1
+    varbose_name_plural = 'Telefones'
+
+
+class EstabelecimentoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', )
+    search_fields = ('name',)
+    inlines = (TelefoneInline, )
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return super(EstabelecimentoAdmin, self).queryset(request)
+        return super(EstabelecimentoAdmin, self).queryset(request).filter(id=request.user.get_profile().estabelecimento.id)
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description','price', 'inativo')
+    search_fields = ('name',)
+
+
+    def queryset(self, request):
+        return super(ProdutoAdmin, self).queryset(request).filter(estabelecimento=request.user.get_profile().estabelecimento)
+
+    def save_model(self, request, obj, form, change):
+        obj.estabelecimento=request.user.get_profile().estabelecimento
+        obj.save()
+
+
+class LinhaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'estabelecimento')
+    search_fields = ('name',)
+
+    def queryset(self, request):
+        return super(LinhaAdmin, self).queryset(request).filter(estabelecimento=request.user.get_profile().estabelecimento)
+
+    def save_model(self, request, obj, form, change):
+        obj.estabelecimento=request.user.get_profile().estabelecimento
+        obj.save()
+
+
+class RamoAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Estabelecimento, EstabelecimentoAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Linha, LinhaAdmin)
+admin.site.register(Telefone, TelefoneAdmin)
+admin.site.register(Ramo, RamoAdmin)
---
+++ TCCweb/core/models.py
@@ -0,0 +1,95 @@
+# coding: utf-8
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.db.models.signals import post_save
+
+class Ramo(models.Model):
+    name = models.CharField(_('Nome'), max_length=50, blank=False)
+
+    class Meta:
+        ordering = [u'name']
+        verbose_name = _(u'ramo')
+        verbose_name_plural = _(u'ramos')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Estabelecimento(models.Model):
+    name = models.CharField(_('Nome'), max_length=50)
+    description = models.TextField(_(u'Descrição'), max_length=200)
+    ramo = models.ManyToManyField(Ramo)
+
+    @property
+    def telefones(self):
+        return Telefone.objects.filter(estabelecimento=self)
+
+    @property
+    def produtos(self):
+        return Produto.objects.filter(estabelecimento=self)
+
+    class Meta:
+        ordering = [u'name']
+        verbose_name = _(u'estabelecimento')
+        verbose_name_plural = _(u'estabelecimentos')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Telefone(models.Model):
+    numero = models.CharField(_('Telefone'), max_length=20, blank=True)
+    estabelecimento = models.ForeignKey(Estabelecimento, editable=False)
+
+    class Meta:
+        ordering = [u'numero']
+        verbose_name = _(u'telefone')
+        verbose_name_plural = _(u'telefones')
+
+    def __unicode__(self):
+        return self.numero
+
+
+class Linha(models.Model):
+    name = models.CharField(_('Nome'), max_length=50, blank=False)
+    estabelecimento = models.ForeignKey(Estabelecimento, editable=False)
+
+    class Meta:
+        ordering = [u'name']
+        verbose_name = _(u'linha')
+        verbose_name_plural = _(u'linhas')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Produto(models.Model):
+    name = models.CharField(_('Nome'), max_length=50)
+    description = models.TextField(_(u'Descrição'), max_length=200)
+    price = models.DecimalField(_(u'Preço'), max_digits=12, decimal_places=2)
+    linha = models.ForeignKey(Linha)
+    estabelecimento = models.ForeignKey(Estabelecimento, editable=False)
+    inativo = models.BooleanField()
+
+    class Meta:
+        ordering = [u'name']
+        verbose_name = _(u'produto')
+        verbose_name_plural = _(u'produtos')
+
+    def __unicode__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    estabelecimento = models.OneToOneField(Estabelecimento, null=True, unique=True)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
