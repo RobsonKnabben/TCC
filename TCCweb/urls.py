310c26bfc1e2b088194415b77149fc9507e2cd56
Robson Knabben <robson.knabben@gmail.com>
2013-09-13 19:42:41 -0300

    Preparando para deploy

--- TCCweb/urls.py
+++ TCCweb/urls.py
@@ -1,11 +1,15 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^api/', include('TCCweb.api.urls', namespace='api')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.views.static',
+    url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
 )
